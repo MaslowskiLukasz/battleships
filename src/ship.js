5c2db0788182f49e5b39c9af4b5a93dbaf314c26
MaslowskiLukasz <l.maslowski2012@gmail.com>
2022-06-08 23:25:08 +0200

    change isSunk getHitMarks to oneliners, add getLength function

--- src/ship.js
+++ src/ship.js
@@ -6,22 +6,19 @@ function shipFactory(shipLength) {
     if(n < 0 || n >= length) {
       throw('hit point out of range');
     }
-
     hitMarks.push(n);
   }
 
-  const isSunk = () => {
-    return hitMarks.length === length;
-  }
-
-  const getHitMarks = () => {
-    return [...hitMarks];
-  }
+  const isSunk = () => hitMarks.length === length;
+  const getHitMarks = () => [...hitMarks];
+  const getLength = () => length;
+  
 
   return {
     hit,
     getHitMarks,
     isSunk,
+    getLength,
   }
 }
 
