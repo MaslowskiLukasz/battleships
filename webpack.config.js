c5335e9d742fb0bd78ff8e2f26148dfcb0fbcb95
MaslowskiLukasz <l.maslowski2012@gmail.com>
2022-06-21 01:16:13 +0200

    add css loader to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,15 @@
 module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
 }
