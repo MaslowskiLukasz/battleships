a3e6264b4c30a283528cd51d690cf6991b944e18
MaslowskiLukasz <l.maslowski2012@gmail.com>
2022-07-15 13:19:14 +0200

    add array with all ship types

---
+++ src/utils.js
@@ -0,0 +1,31 @@
+/*
+Carrier 	5
+Battleship 	4
+Destroyer 	3
+Submarine 	3
+Patrol Boat 	2 
+*/
+const ships = [
+  {
+    name: 'carrier',
+    length: 5,
+  },
+  {
+    name: 'battleship',
+    length: 4,
+  },
+  {
+    name: 'destroyer',
+    length: 3,
+  },
+  {
+    name: 'submarine',
+    length: 3,
+  },
+  {
+    name: 'patrol boat',
+    length: 2
+  },
+];
+
+export default ships;
