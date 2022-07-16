53a0afe722695b0c32c122d22b0c0e33db77eb7a
MaslowskiLukasz <l.maslowski2012@gmail.com>
2022-07-17 00:39:53 +0200

    move console log hits and winner from game.js

--- src/UIUtils.js
+++ src/UIUtils.js
@@ -1,10 +1,8 @@
-import gameboardFactory from "./gameboard";
-
 const initUI = () => {
   const body = document.body;
   const wrapper = document.createElement('div');
   wrapper.appendChild(createPlayerScreen('AI', true));
-  wrapper.appendChild(createPlayerScreen('Player', [], false));
+  wrapper.appendChild(createPlayerScreen('Player', false));
 
   body.appendChild(wrapper);
 }
@@ -13,6 +11,12 @@ const createPlayerScreen = (name, isAI) => {
   const wrapper = document.createElement('div');
   const playerName = document.createElement('h2');
   const board = createBoard(isAI);
+
+  if (isAI) {
+    board.id = 'ai-board';
+  } else {
+    board.id = 'player-board';
+  }
   
   playerName.textContent = name;
 
@@ -34,11 +38,10 @@ const createBoard = (isInteractive) => {
   for (let y = 0; y < 10; y++) {
     for (let x = 0; x < 10; x++) {
       const area = document.createElement('button');
-      area.disabled = !isInteractive;
+      //area.disabled = !isInteractive;
       area.className = 'area';
       area.setAttribute('x', x);
       area.setAttribute('y', y);
-      area.addEventListener('click', getCoordinates);
 
       wrapper.appendChild(area);
     }
@@ -47,9 +50,13 @@ const createBoard = (isInteractive) => {
   return wrapper;
 }
 
-const renderShips = (shipPlacement) => {
-  console.log(shipPlacement);
-  const playerBoard = document.getElementById('playerBoard');
+const renderShips = (shipPlacement, isAI) => {
+  let playerBoard;
+  if (isAI) {
+    playerBoard = document.getElementById('ai-board');
+  } else {
+    playerBoard = document.getElementById('player-board');
+  }
   shipPlacement.map((currShip) => {
     const startX = currShip.x;
     const startY = currShip.y;
@@ -58,26 +65,57 @@ const renderShips = (shipPlacement) => {
       for (let y = startY; y < startY + currShip.ship.getLength(); y++) {
         const area = playerBoard.querySelector(`[x="${startX}"][y="${y}"]`);
         area.textContent = 'X';
-        console.log(area);
       }
     } else {
       for (let x = startX; x < startX + currShip.ship.getLength(); x++) {
         const area = playerBoard.querySelector(`[x="${x}"][y="${startY}"]`);
         area.textContent = '0';
-        console.log(area);
       }
     }
   });
 }
 
-/*TODO:
-event listener to place ship
-return x, y
-*/
-const getCoordinates = (event) => {
-  const x = event.target.getAttribute('x');
-  const y = event.target.getAttribute('y');
+const getCoords = (area) => {
+  const x = parseInt(area.getAttribute('x'));
+  const y = parseInt(area.getAttribute('y'));
+
   return { x: x, y: y }
 }
 
-export default initUI;
+
+
+const logWinner = (playerGameboard, AIGameboard) => {
+  if(playerGameboard.areAllShipsSunk()) {
+    console.log('AI won');
+    console.log(
+      `Player hit: ${AIGameboard.getHits().length} times.
+      AI hit: ${playerGameboard.getHits().length} times`
+    );
+  } else {
+    console.log('Player won');
+    console.log(
+      `Player hit: ${AIGameboard.getHits().length} times.
+      AI hit: ${playerGameboard.getHits().length} times`
+    );
+  }
+}
+
+const logStatus = (playerGameboard, AIGameboard) => {
+  console.log(
+    `Player hit: ${AIGameboard.getHits().length} times.
+    Player missed: ${AIGameboard.getMisses().length} times`
+  );
+
+  console.log(
+    `AI hit: ${playerGameboard.getHits().length} times.
+    AI missed: ${playerGameboard.getMisses().length} times`
+  );
+}
+
+export {
+  initUI, 
+  renderShips,
+  getCoords,
+  logStatus,
+  logWinner,
+};
